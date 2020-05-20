99490302554426085aba03270c3e867fd0a7b341
Vera <lizeth.veraro@outlook.com>
2020-05-20 16:15:38 -0700

    routes for login and logout user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
+
   root to: "pages#home"
   resources :works
 end
