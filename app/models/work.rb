64e30d9d2db06a8398c805056ece07dbd41b9d63
Vera <lizeth.veraro@outlook.com>
2020-05-20 13:17:02 -0700

    Top ten per category, peding the top ten with votes

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,10 @@
 class Work < ApplicationRecord
+  def self.top_ten(category)
+    all_works = self.all
+    ten_works = all_works.select do |work|
+      work.category == category
+    end
+
+    return ten_works
+  end
 end
