01980279d1c3fd45f23f6e4dea06475a93f7f83b
Vera <lizeth.veraro@outlook.com>
2020-05-20 13:18:49 -0700

    get rid off extra comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)require "csv"
 
 require "csv"
 
@@ -30,51 +23,6 @@ end
 
 puts "Added #{Work.count} work records"
 puts "#{work_failures.length} works failed to save"
-
-# PASSENGER_FILE = Rails.root.join("db", "seed_data", "passengers.csv")
-# puts "Loading raw passenger data from #{PASSENGER_FILE}"
-
-# passenger_failures = []
-# CSV.foreach(PASSENGER_FILE, :headers => true) do |row|
-#   passenger = Passenger.new
-#   passenger.id = row["id"]
-#   passenger.name = row["name"]
-#   passenger.phone_num = row["phone_num"]
-#   successful = passenger.save
-#   if !successful
-#     passenger_failures << passenger
-#     puts "Failed to save passenger: #{passenger.inspect}"
-#   else
-#     puts "Created passenger: #{passenger.inspect}"
-#   end
-# end
-
-# puts "Added #{Passenger.count} passenger records"
-# puts "#{passenger_failures.length} passengers failed to save"
-
-# TRIP_FILE = Rails.root.join("db", "seed_data", "trips.csv")
-# puts "Loading raw trip data from #{TRIP_FILE}"
-
-# trip_failures = []
-# CSV.foreach(TRIP_FILE, :headers => true) do |row|
-#   trip = Trip.new
-#   trip.id = row["id"]
-#   trip.driver_id = row["driver_id"]
-#   trip.passenger_id = row["passenger_id"]
-#   trip.date = Date.strptime(row["date"], "%Y-%m-%d")
-#   trip.rating = row["rating"]
-#   trip.cost = row["cost"]
-#   successful = trip.save
-#   if !successful
-#     trip_failures << trip
-#     puts "Failed to save trip: #{trip.inspect}"
-#   else
-#     puts "Created trip: #{trip.inspect}"
-#   end
-# end
-
-# puts "Added #{Trip.count} trip records"
-# puts "#{trip_failures.length} trips failed to save"
 
 # Since we set the primary key (the ID) manually on each of the
 # tables, we've got to tell postgres to reload the latest ID
