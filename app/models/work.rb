9b6a876cd5852003d4d84889c4a2dca314cc6301
Vera <lizeth.veraro@outlook.com>
2020-05-20 14:59:41 -0700

    validates the title presence and uniqueness

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,7 @@
 class Work < ApplicationRecord
+
+  validates :title, presence: true, uniqueness: true
+  
   def self.top_ten(category)
     all_works = self.all
     ten_works = all_works.select do |work|
