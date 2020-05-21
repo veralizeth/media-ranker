6a312591964e32787311f3977242e39c1c244bda
Vera <lizeth.veraro@outlook.com>
2020-05-21 15:54:09 -0700

    top ten and spotlight methods

--- app/models/work.rb
+++ app/models/work.rb
@@ -7,12 +7,33 @@ class Work < ApplicationRecord
                                     too_long: "%{count} characters is the maximum allowed" }
   validates :publication_year, numericality: { greater_than: 0 }
 
-  def self.top_ten(category)
+  def self.all_work(category)
     all_works = self.all
     ten_works = all_works.select do |work|
       work.category == category
     end
-
     return ten_works
+  end
+
+  def self.top_ten(category)
+    all_works = self.all
+    voted_works = all_works.select do |work|
+      work.category == category && work.votes.count > 0
+    end
+    # Minus -work to order the results descending. 
+    top = voted_works.sort_by { |work| -work.votes.length }
+
+    top.length < 10 ? top : top[0..9]
+  end
+
+  def self.spotlight
+    all_works = self.all
+    spotlight = all_works.select do |work|
+      work.votes.count > 0
+    end
+    # Minus -work to order the results descending.
+    top = spotlight.sort_by { |work| -work.votes.length }
+
+    return top[0]
   end
 end
