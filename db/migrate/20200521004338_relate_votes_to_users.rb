055edb28d8d272a3fdf6122cd041a428a0d7728e
Vera <lizeth.veraro@outlook.com>
2020-05-20 17:49:03 -0700

    DB migrations to link votes with users and works

---
+++ db/migrate/20200521004338_relate_votes_to_users.rb
@@ -0,0 +1,5 @@
+class RelateVotesToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :votes, :user, index: true
+  end
+end
---
+++ db/migrate/20200521004346_relate_votes_to_works.rb
@@ -0,0 +1,5 @@
+class RelateVotesToWorks < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :votes, :work, index: true
+  end
+end
