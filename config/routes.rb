514c7283af22a95a6b4d9c8a5764d2ec841d2156
Vera <lizeth.veraro@outlook.com>
2020-05-21 01:02:10 -0700

    adding routes to votes and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+
+  resources :works do
+    resources :votes, only: [:create]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
   get "/users/current", to: "users#current", as: "current_user"
 
-  root to: "pages#home"
-  resources :works
+  resources :users
+
 end
