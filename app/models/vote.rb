36f723d54f8ff1980f06c3dd9aa8730e1467db21
Vera <lizeth.veraro@outlook.com>
2020-05-21 22:03:19 -0700

    user_id and work_id validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,7 @@ class Vote < ApplicationRecord
   belongs_to :work
   # it can also validate whether the value of the specified attributes
   # are unique based on multiple scope parameters.
-  validates :user_id, uniqueness: { scope: :work_id }
+  validates :user_id, presence: true, uniqueness: { scope: :work_id }
+  validates :work_id, presence: true
+  
 end
